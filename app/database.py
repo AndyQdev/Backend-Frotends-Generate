0aad95727e2869c6bb6974e6630a25c8ef4aca4e
Andres <andres.03.12.2003@gmail.com>
2025-04-30 01:13:38 -0400

    Actualizar la URL de conexión a la base de datos PostgreSQL en database.py

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ import os
 
 # 🚨 Configura tu URL de PostgreSQL aquí
 # DATABASE_URL = os.getenv("DATABASE_URL", DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/backend_app")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/backend_app")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://bd_gnerate_frontends_user:xDLVHjaqX1JaFHq8PrRgjKMI45lGN2Iw@dpg-d08quvs9c44c73dpmt1g-a.oregon-postgres.render.com/bd_gnerate_frontends")
 
 
 # Crear engine
